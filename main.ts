b0a04b29034302fe2a478ae41320b683d68328b6
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-28 23:44:15 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.temperature())
-    if (input.temperature() > 25) {
+let temperatura = 0
+basic.forever(function () {
+    temperatura = input.temperature()
+    basic.showNumber(temperatura)
+    if (temperatura > 25) {
         basic.showIcon(IconNames.Happy)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        basic.showIcon(IconNames.Sad)
         pins.digitalWritePin(DigitalPin.P0, 0)
     }
 })
